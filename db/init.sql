e672544f8e2fcddfc675991303f422f43c10fcd3
schmigo <jannik.schmidt@hs-augsburg.de>
2023-04-18 17:22:53 +0200

    docker-compose.yml überarbeitet

---
+++ db/init.sql
@@ -0,0 +1,59 @@
+-- Erstellen der Benutzertabelle
+CREATE TABLE users (
+  user_id SERIAL PRIMARY KEY,
+  email VARCHAR(255) UNIQUE NOT NULL,
+  password VARCHAR(255) NOT NULL,
+  role VARCHAR(255) NOT NULL,
+  created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
+
+-- Erstellen der Gästetabelle
+CREATE TABLE guests (
+  guest_id SERIAL PRIMARY KEY,
+  user_id INTEGER NOT NULL,
+  first_name VARCHAR(255) NOT NULL,
+  last_name VARCHAR(255) NOT NULL,
+  phone_number VARCHAR(255),
+  FOREIGN KEY (user_id) REFERENCES users (user_id)
+);
+
+-- Erstellen der Restauranttabelle
+CREATE TABLE restaurants (
+  restaurant_id SERIAL PRIMARY KEY,
+  user_id INTEGER NOT NULL,
+  name VARCHAR(255) NOT NULL,
+  address VARCHAR(255) NOT NULL,
+  phone_number VARCHAR(255) NOT NULL,
+  opening_hours VARCHAR(255) NOT NULL,
+  created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  FOREIGN KEY (user_id) REFERENCES users (user_id)
+);
+
+-- Erstellen der Reservierungstabelle
+CREATE TABLE reservations (
+  reservation_id SERIAL PRIMARY KEY,
+  guest_id INTEGER NOT NULL,
+  restaurant_id INTEGER NOT NULL,
+  date DATE NOT NULL,
+  time TIME NOT NULL,
+  party_size INTEGER NOT NULL,
+  status VARCHAR(255) NOT NULL,
+  created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  FOREIGN KEY (guest_id) REFERENCES guests (guest_id),
+  FOREIGN KEY (restaurant_id) REFERENCES restaurants (restaurant_id)
+);
+
+-- Erstellen der Verfügbarkeitstabelle für Restaurants
+CREATE TABLE restaurant_availabilities (
+  availability_id SERIAL PRIMARY KEY,
+  restaurant_id INTEGER NOT NULL,
+  start_time TIME NOT NULL,
+  end_time TIME NOT NULL,
+  max_party_size INTEGER NOT NULL,
+  created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  FOREIGN KEY (restaurant_id) REFERENCES restaurants (restaurant_id)
+);
