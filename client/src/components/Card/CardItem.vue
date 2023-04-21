41f52b4ae6488fdd3ede6e46439a52060721045f
taichi-de <61309248+taichi-de@users.noreply.github.com>
2023-04-21 15:51:04 +0200

    added carditem component

---
+++ client/src/components/Card/CardItem.vue
@@ -0,0 +1,25 @@
+<script setup></script>
+
+<template>
+  <div class="bg-white hover:bg-gray-100 rounded-lg shadow-md overflow-hidden">
+    <img class="w-full h-48 object-cover" src="/cafe1.jpeg" />
+    <div class="p-4">
+      <h2 class="text-xl font-medium">Cafe DEMO</h2>
+      <p class="text-gray-600 text-sm my-2">
+        a small chill cafe in the central of Augsburg
+      </p>
+      <div class="flex">
+        <button
+          class="bg-pink-500 hover:bg-pink-600 text-white my-2 mr-2 px-4 py-2 rounded-lg focus:outline-none"
+        >
+          ◎ 18:00
+        </button>
+        <button
+          class="bg-pink-500 hover:bg-pink-600 text-white my-2 px-4 py-2 rounded-lg focus:outline-none"
+        >
+          △ 18:30
+        </button>
+      </div>
+    </div>
+  </div>
+</template>
