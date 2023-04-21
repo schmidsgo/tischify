44f1b64ef7702eb801f830ea42d43189e4aac1eb
taichi-de <61309248+taichi-de@users.noreply.github.com>
2023-04-21 15:52:38 +0200

    added Navbar component

---
+++ client/src/components/Navbar.vue
@@ -0,0 +1,19 @@
+<script setup></script>
+
+<template>
+  <div
+    class="flex items-center justify-between py-4 px-8 bg-gray-800 drop-shadow-md"
+  >
+    <div class="flex items-center">
+      <img class="h-12 mr-4" src="logo.svg" alt="Logo" />
+    </div>
+    <div class="flex items-center">
+      <button class="text-white bg-blue-400 rounded py-2 px-4 font-medium mr-2">
+        Login
+      </button>
+      <button class="text-white bg-blue-400 rounded py-2 px-4 font-medium">
+        Sign up
+      </button>
+    </div>
+  </div>
+</template>
