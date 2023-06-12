b049e51a6a83a2226a85b7456dbc410beda3d15d
taichi-de <61309248+taichi-de@users.noreply.github.com>
2023-06-12 17:34:45 +0200

    refactored

--- client/src/components/Card/BookingModal.vue
+++ client/src/components/Card/BookingModal.vue
@@ -1,44 +1,48 @@
 <script setup lang="ts">
-import axios from 'axios';
-import { ref } from 'vue';
+import { computed, reactive, ref } from 'vue';
 import { useAuthStore, useBookingStore } from '../../stores/state';
 import type { ItemType } from '../../types/types';
 
-defineProps<{ item: ItemType }>();
+const props = defineProps<{ item: ItemType; itemId: string }>();
 
 const authStore = useAuthStore();
 const bookingStore = useBookingStore();
+
+const store = reactive({
+  item: {} as ItemType
+});
+
+const item = computed(() => {
+  if (props.item.restaurant_id === props.itemId)
+    return { ...props.item, restaurant_id: props.itemId };
+  return null;
+});
+
+// watch(restaurant_id, id => {
+//   if (id) {
+//     bookingStore.closeModal();
+//   }
+// });
 
 const name = authStore.user.name !== '' ? authStore.user.name : ref('');
 const email = ref('');
 const people = ref(0);
 const datetime = ref('');
-const isError = ref(false);
-const isLoading = ref(false);
 
 const book = async () => {
-  try {
-    isLoading.value = true;
-    const response = await axios.post('http://localhost:3000/bookings', {
-      name: name.valueOf,
-      email: email.value,
-      people: people.value,
-      datetime: datetime.value
-    });
-    bookingStore.submitBooking(response.data);
-    bookingStore.showBookingModal = false;
-  } catch (error) {
-    isError.value = true;
-  } finally {
-    isLoading.value = false;
-  }
+  await bookingStore.submitBooking({
+    restaurant_id: store.item.restaurant_id,
+    name: name.value,
+    email: email.value,
+    people: people.value,
+    datetime: datetime.value
+  });
 };
 </script>
 
 <template>
   <v-dialog
     v-model="bookingStore.showBookingModal"
-    persistent
     width="70%"
     scrim="grey-darken-4"
   >
@@ -46,22 +50,27 @@ const book = async () => {
       <v-row>
         <v-col cols="6" justify="start" class="bg-yellow">
           <v-card-image>
-            <v-img :src="`${item.category}.jpeg`" width="100%" />
+            <v-img
+              :src="`${item.category}.jpeg`"
+              width="100%"
+              class="bg-grey-lighten-1 rounded-lg"
+            />
           </v-card-image>
           <v-card-title class="text-lg font-weight-bold mt-4 mb-2 pa-0">
             {{ item.name }}
             <v-divider thickness="2" class="border-opacity-75 mt-1 w-50" />
           </v-card-title>
           <v-card-text class="text-lg mt-2 pa-0">
-            {{ item.address }}, 86150, {{ item.city }}
+            {{ item.address }}, {{ item.city }}
           </v-card-text>
           <div class="d-flex align-center justify-start w-50 mt-2">
             <v-card-text class="text-lg pa-0">
               {{ item.opening_hours }}
             </v-card-text>
             <v-card-text class="text-lg text-blue-darken-2 pa-0">
-              <v-icon icon="mdi-phone" size="small" class="mr-2"></v-icon
-              >{{ item.phone_number }}
+              <v-icon icon="mdi-phone" size="small" class="mr-2" />{{
+                item.phone_number
+              }}
             </v-card-text>
           </div>
         </v-col>
@@ -71,9 +80,10 @@ const book = async () => {
               Book {{ item.name }}
             </v-card-text>
             <v-card-actions class="justify-end">
+              <!-- FIXME -->
               <v-btn
                 icon="mdi-close"
-                @click="bookingStore.showBookingModal = false"
+                @click="bookingStore.closeModal"
                 class="bg-grey-lighten-3 text-blue rounded-circle pa-0"
               />
             </v-card-actions>
@@ -85,29 +95,33 @@ const book = async () => {
                 v-model="bookingStore.booking.name"
                 outlined
                 type="text"
+                required
               />
               <v-text-field
                 placeholder="Email"
                 v-model="bookingStore.booking.email"
                 outlined
                 type="email"
+                required
               />
               <v-text-field
                 placeholder="Personen"
                 v-model="bookingStore.booking.people"
                 outlined
                 type="number"
+                required
               />
               <v-text-field
                 placeholder="Datum und Uhrzeit"
                 v-model="bookingStore.booking.datetime"
                 type="datetime-local"
                 outlined
+                required
               />
-              <p v-if="isError" class="text-red ml-4">Error!</p>
+              <p v-if="authStore.isError" class="text-red ml-4">Error!</p>
               <v-card-actions class="justify-end">
                 <v-btn
-                  v-if="!isLoading"
+                  v-if="!authStore.isLoading"
                   color="info"
                   variant="flat"
                   size="large"
@@ -117,7 +131,7 @@ const book = async () => {
                   buchen
                 </v-btn>
                 <v-btn
-                  v-else-if="isLoading"
+                  v-else-if="authStore.isLoading"
                   color="Blue"
                   variant="flat"
                   class="p-2"
